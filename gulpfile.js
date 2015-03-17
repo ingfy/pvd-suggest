23834d1cd8f37b39d6421647a794616212b6845a
yngsva <yngve.svalestuen@ciber.com>
2015-03-17 16:05:08 +0100

    Browserify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var gulp   = require('gulp');
+var gulp    = require('gulp');
+var del     = require('del');
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
+var uglify = require('gulp-uglify');
 var plugins = require('gulp-load-plugins')();
 
 var paths = {
@@ -9,6 +13,8 @@ var paths = {
   tests: ['./test/**/*.js', '!test/{temp,temp/**}']
 };
 
+var moduleName = 'pvd-suggest';
+
 var plumberConf = {};
 
 if (process.env.CI) {
@@ -16,6 +22,26 @@ if (process.env.CI) {
     throw err;
   };
 }
+
+gulp.task('clean-dist', function () {
+    return del(['dist/']);
+});
+
+gulp.task('browserify', ['clean-dist'], function () {
+  // transform regular node stream to gulp (buffered vinyl) stream
+
+  var browserified = transform(function(filename) {
+    var b = browserify();
+    b.require(filename, {expose: moduleName})
+    return b.bundle();
+  });
+
+  return gulp.src('./index.js')
+    .pipe(browserified)
+        // Add transformation tasks to the pipeline here.
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('lint', function () {
   return gulp.src(paths.lint)
